46f811e7dc6376751424127b2d4a7388bcf63f07
Alex Reshetnyak <alexreshetnyakmail@gmail.com>
2017-05-04 15:11:59 +0300

    complet 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
   src = 'https://plnkr.co/edit/1eawJZ4eJ4KfUDxBMr1D?p=preview';
+  links: Object[] = [
+        { path: '/dashboard', icon: 'home', label: 'Главная'},
+        { path: '/task', icon: 'event_name', label: 'Задачи'},
+        { path: '/statistics', icon: 'settings', label: 'Статистика'}
+    ];
 }
